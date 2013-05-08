6d75f49a3446bc34fc12f11cadf100d879d69f03
Jamie Iles <jamie@jamieiles.com>
2013-05-08 17:55:45 +0100

    Initial commit of SDR SDRAM controller.
    Simple SDR SDRAM controller for 4Mx16x4 devices, e.g. ISSI IS45S16160G
    (32MB).
    
    This controller performs simple 16 bit accesses with one-hot byte
    enables.  There is no burst support and all accesses are performed as a
    single access with auto precharge so this is definitely not the most
    optimal scheme.
    
    Signed-off-by: Jamie Iles <jamie@jamieiles.com>

---
+++ counter.v
@@ -0,0 +1,19 @@
+module counter(input wire clk,
+	       output wire [count_width - 1:0] count,
+	       input wire reset);
+
+parameter count_width = 8;
+parameter count_max = 255;
+
+reg [count_width - 1:0] count_reg = 0;
+
+always @(posedge clk) begin
+	if (reset)
+		count_reg <= 0;
+	else if (count_reg != count_max)
+		count_reg <= count + 1'b1;
+end
+
+assign count = count_reg;
+
+endmodule
